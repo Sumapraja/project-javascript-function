f660854f6be43adbbfb8bdd95cae01dc73c6227f
Sumapraja <irfansumapraja@gmail.com>
2019-06-26 14:39:02 +0700

    Added function and if for horoscope reading

--- index.js
+++ index.js
@@ -1,56 +1,36 @@
-function logName (name) {
-console.log(`hello, ${name}!`);
+let Name = prompt(`Welcome! dear the chosen one, what's your name?`)
+alert(`Hello ${Name}. i'll get your horoscope reading. can i have your details?`)
+let BirthMonth = prompt(`What Month were you born?`)
+let BirthDate = parseInt(prompt(`What date were you born?`))
+
+const checkZodiac = function (month, date) {
+        if ((month == 'December' && date >= 22 && date <= 31) || (month == 'January' && date >= 1 && date <= 19)) {
+            alert('You are a Capricorn');
+        } else if ((month == 'January' && date >= 20 && date <= 31) || (month == 'February' && date >= 1 && date <= 18)) {
+            alert('You are a Aquarius');
+        } else if ((month == 'February' && date >= 19 && date <= 29) || (month == 'March' && date >= 1 && date <= 20)) {
+            alert('You are a Pisces');
+        } else if ((month == 'March' && date >= 21 && date <= 31) || (month == 'April' && date >= 1 && date <= 19)) {
+            alert('You are a Aries');
+        } else if ((month == 'April' && date >= 20 && date <= 30) || (month == 'May' && date >= 1 && date <= 20)) {
+            alert('You are a Taurus');
+        } else if ((month == 'May' && date >= 21 && date <= 31) || (month == 'June' && date >= 1 && date <= 20)) {
+            alert('You are a Gemini');
+        } else if ((month == 'June' && date >= 21 && date <= 30) || (month == 'July' && date >= 1 && date <= 22)) {
+            alert('You are a Cancer');
+        } else if ((month == 'July' && date >= 23 && date <= 31) || (month == 'August' && date >= 1 && date <= 22)) {
+            alert('You are a Leo');
+        } else if ((month == 'August' && date >= 23 && date <= 31) || (month == 'September' && date >= 1 && date <= 22)) {
+            alert('You are a Virgo');
+        } else if ((month == 'September' && date >= 23 && date <= 30) || (month == 'October' && date >= 1 && date <= 22)) {
+            alert('You are a Libra');
+        } else if ((month == 'October' && date >= 22 && date <= 31) || (month == 'November' && date >= 1 && date <= 21)) {
+            alert('You are a Scorpio');
+        } else if ((month == 'November' && date >= 22 && date <= 30) || (month == 'December' && date >= 1 && date <= 21)) {
+            alert('You are a Sagittarius');
+        } else (alert(`Oops!! Something's wrong.
+    \n Please input your birth month in english spellings and first letter in uppercase
+    \n Example : "January" `))
 }
 
-const nameA = 'Irfan'
-const nameB = 'Risyad'
-
-logName(nameA)
-logName(nameB)
-
-const checkAge = function(age) {
-    if (age>= 18){
-        console.log('You are old enough');        
-    } else if (age < 18 && age >= 0) {
-        console.log('You are still young');
-    } else {
-        console.log('You are not born yet');        
-    }
-}
-
-checkAge(10)
-checkAge(25)
-checkAge(-2)
-
-const addMarks = (text, mark, times) => {
-    let newText = text
-  
-    for (let i = 0; i <= times; i++) {
-      newText += mark
-    }
-  
-    console.log(newText)
-  }
-  
-  addMarks('Hello', '!', 1)
-  addMarks('Hello', '!', 3)
-  addMarks('How are you', '?', 1)
-  addMarks('How are you', '?', 2)
-  
-  const showNameWithAge = (name = 'Unknown', age = 0) => {
-    const nameWithAge = `${name} is ${age} year(s) old`
-  
-    return nameWithAge
-  }
-  
-  const alpha = showNameWithAge('Alpha', 11)
-  const betty = showNameWithAge('Betty', 42)
-  const gamma = showNameWithAge('Gamma', 30)
-  
-  const hi = showNameWithAge()
-
-  console.log(alpha)
-  console.log(betty)
-  console.log(gamma)
-  console.log(hi);
-  
+checkZodiac(BirthMonth, BirthDate)
